af4064daa15f1006910e5d8d776a0f12d4098b3e
Lavanya Kommaraju <70351093+Lavanyamca22@users.noreply.github.com>
2021-06-02 18:37:16 +0530

    Update (12)_2D_Arrays.py
    Sample Input
    1 1 1 0 0 0
    0 1 0 0 0 0
    1 1 1 0 0 0
    0 0 2 4 4 0
    0 0 0 2 0 0
    0 0 1 2 4 0
    
    Sample Output
    19
    
    Explanation
    A contains the following hourglasses:
    
    1 1 1   1 1 0   1 0 0   0 0 0
      1       0       0       0
    1 1 1   1 1 0   1 0 0   0 0 0
    
    0 1 0   1 0 0   0 0 0   0 0 0
      1       1       0       0
    0 0 2   0 2 4   2 4 4   4 4 0
    
    1 1 1   1 1 0   1 0 0   0 0 0
      0       2       4       4
    0 0 0   0 0 2   0 2 0   2 0 0
    
    0 0 2   0 2 4   2 4 4   4 4 0
      0       0       2       0
    0 0 1   0 1 2   1 2 4   2 4 0
    The hourglass with the maximum sum () is:
    2 4 4
      2
    1 2 4

--- (12)_2D_Arrays.py
+++ (12)_2D_Arrays.py
@@ -1,8 +1,8 @@
 from itertools import chain
 if __name__ == '__main__':
     Array = [list(map(int,input().split())) for i in range(6)]
-    expand_array = []
+    sum_expand_array = []
     for i in range(0,4):
         for j in range(0,4):
-            expand_array.append(sum(list(chain.from_iterable([Array[i][j:j+3],[Array[i+1][j+1]],Array[i+2][j:j+3]]))))
-    print(max(expand_array))
+            sum_expand_array.append(sum(list(chain.from_iterable([Array[i][j:j+3],[Array[i+1][j+1]],Array[i+2][j:j+3]]))))
+    print(max(sum_expand_array))
